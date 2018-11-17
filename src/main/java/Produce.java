33b0ddefb70fcae09b3b6f8871db7d600f54c3c8
SugarChen <chunqiuxuyu@126.com>
2018-11-17 21:01:01 +0800

    测试

--- src/main/java/Produce.java
+++ src/main/java/Produce.java
@@ -1,11 +1,16 @@
 import impl.SendMailFactory;
+import impl.SendSmsFactory;
 import inf.Provider;
 import inf.Sender;
 
 public class Produce {
     public static void main(String[] args) {
-        Provider provider = new SendMailFactory();
-        Sender sender = provider.produce();
-        sender.Send();
+        Provider provider1 = new SendMailFactory();
+        Sender sender1 = provider1.produce();
+        sender1.Send();
+
+        Provider provider2 = new SendSmsFactory();
+        Sender sender2 = provider2.produce();
+        sender2.Send();
     }
 }
