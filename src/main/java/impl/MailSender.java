d45fc0f7b52a163d10952875c1e645423134880a
SugarChen <chunqiuxuyu@126.com>
2018-11-17 22:33:04 +0800

    测试

--- src/main/java/impl/MailSender.java
+++ src/main/java/impl/MailSender.java
@@ -4,6 +4,6 @@ import inf.Sender;
 
 public class MailSender implements Sender {
     public void Send() {
-        System.out.println("this is mail sender!");
+        System.out.println("This is mail sender!");
     }
 }
